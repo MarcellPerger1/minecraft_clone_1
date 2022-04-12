e7c970ff8b96b8d066f75141e74bcd8c2580ba1d
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-04-12 18:27:04 +0000

    Atlas generation working

--- scripts/make_atlas.js
+++ scripts/make_atlas.js
@@ -5,17 +5,28 @@ const { pipeline } = require('stream/promises');
 const {createCanvas, loadImage} = require('canvas');
 
 
-function readFromDir(dir){
+function readFromDir(dir, c){
   return fsP.readdir(dir)
-    .then((files) => {
-      return files.map((f) => readImage(f))
+    .then(async (files) => {
+      let imgs = [];
+      for(const f of files){
+        let img = await readImage(path.join(dir, f));
+        if(img!=null){
+          imgs.push(img);
+        }
+      }
+      return imgs;
     })
 }
 
-function readImage(f){
+async function readImage(f, c){
   return fsP.stat(f).then((st) => {
-    if(st.isFile() && f.endsWith('.min.png')){
-      return loadImage(f);
+    if(st.isFile() && f.endsWith('.min.png') && !f.endsWith('atlas.min.png')){
+      let v = loadImage(f);
+      if(c!=null){
+        v = v.then(c);
+      }
+      return v;
     }
   });
 }
@@ -31,18 +42,18 @@ function getTexturesDir(trustCwd=true){
 function main(){
   const texDir = getTexturesDir();
   var canv, ctx, n;
-  cnosole.log('Loading images..')
-  readFromDir(texDir).then((images) => {
+  console.log('Loading images...')
+  return readFromDir(texDir).then((images) => {
     console.log('Creating canvas...')
     n = images.length;
     canv = createCanvas(n*16, 16);
     ctx = canv.getContext('2d');
-    console.log('Drawing images..')
-    for(const [i, img] of Object.entries(images)){
-      ctx.drawImage(img, i*16, 0);
-    }
+    console.log('Drawing images...');
+    return Promise.all(
+      images.map((img, i) => ctx.drawImage(img, i*16, 0))
+    )
   }).then((_r) => {
-    console.log('Creating atlas..');
+    console.log('Creating atlas...');
     const out = fs.createWriteStream(path.join(texDir, 'atlas.min.png'));
     const png_stream = canv.createPNGStream();
     pipeline(png_stream, out).then((_res) => {
@@ -53,3 +64,5 @@ function main(){
 
 console.log('Hello nodejs');
 console.log(`dirname: ${getTexturesDir()}`);
+
+main();
