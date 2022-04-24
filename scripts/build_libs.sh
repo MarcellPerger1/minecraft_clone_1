6b241640eb7331265ec809c6bc326aad80f16f76
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-04-24 12:31:03 +0100

    Update build_libs.sh

--- scripts/build_libs.sh
+++ scripts/build_libs.sh
@@ -1,4 +1,6 @@
 npm install
 # replit doesn't serve node_modules/
+mkdir ./libs
 cp -r ./node_modules/simplex-noise ./libs/simplex-noise
+mkdir ./src/libs
 cp -r ./libs/simplex-noise/dist/esm/ ./src/libs/simplex-noise
