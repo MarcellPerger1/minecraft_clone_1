4624977a93613ab3e3ced32bb713ac7cb49179e9
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-07-02 18:04:17 +0000

    Add some tests for single-arg deepMerge

--- test/deep_merge.test.js
+++ test/deep_merge.test.js
@@ -1,8 +1,26 @@
-import {deepMerge} from "../src/utils/deep_merge.js";
- 
+import { deepMerge } from "../src/utils/deep_merge.js";
+
 describe("deepMerge", () => {
-  it("Returns primitives directly", () => {
-    expect(deepMerge([1])).toBe(1);
-    expect(deepMerge(["abc"])).toBe("abc");
+  describe("Shallow primitive handling", () => {
+    it("Returns numbers directly", () => {
+      for (const n of [0, -12, 9.7, NaN, Infinity]) {
+        expect(deepMerge([n])).toBe(n);
+      }
+    })
+    it("Returns strings directly", () => {
+      for (const s of ["", "abc", "\x1b[0m\\+]"]) {
+        expect(deepMerge([s])).toBe(s);
+      }
+    })
+    it("Returns booleans directly", () => {
+      for (const b of [true, false]) {
+        expect(deepMerge([b])).toBe(b);
+      }
+    })
+    it("Returns null-ish directly", () => {
+      for (const n of [null, void 0]){
+        expect(deepMerge([n])).toBe(n)
+      }
+    })
   })
 })
