c09b0564eba1abbe951e01120d047330bd675853
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-07-19 09:16:48 +0000

    Update count_lines.sh

--- scripts/count_lines.sh
+++ scripts/count_lines.sh
@@ -1,1 +1,10 @@
-find ./src -name "*.js" ! -wholename "**/libs/**" | xargs wc -l | tail -n1
+find . ! -wholename "**/libs/**" ! -wholename "**/node_modules/**" ! -wholename "**/res/**" ! -name "package-lock.json" \
+\( -name "*.js" \
+  -o -name "*.[mc]js" \
+  -o -name "*.json" \
+  -o -name "*.css" \
+  -o -name "*.html" \
+  -o -name "*.sh" \
+  -o -name "*.yml" \
+  -o -name "*.glsl" \
+\) | xargs wc -l | tail -n1
