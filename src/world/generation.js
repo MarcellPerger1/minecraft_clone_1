eac4d4bb7caf48cc32dcc39935013b18af40c1af
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-08-10 16:21:11 +0000

    Use new `World.*` util methods

--- src/world/generation.js
+++ src/world/generation.js
@@ -56,31 +56,27 @@ export class WorldGenerator extends GameComponent {
   }
 
   placeTree(x, z) {
-    const setRelMaybe = (pos, block) => {
+    const setRelOr = (pos, block) => {
       let p = vec3.add([], [x, terrainY, z], pos);
-      return (this.w.inRange(p) 
-        ? this.w.setBlock(p, block) 
-        : Blocks.air);
+      return this.w.setBlockOr(p, block);
     }
-    const getRelOrAir = (pos) => {
+    const getRelOr = (pos) => {
       let p = vec3.add([], [x, terrainY, z], pos);
-      return (this.w.inRange(p) 
-        ? this.w.getBlock(p) 
-        : Blocks.air);
+      return this.w.getBlockOr(p, Blocks.air);
     }
     let terrainY = this.landHeights[x][z];
     for(let yo=3;yo<=5;yo++){
       let r = yo==5 ? 1 : 2;
       for(let xo=-r;xo<=r;xo++) {
         for(let zo=-r;zo<=r;zo++) {
-          if(getRelOrAir([xo, yo, zo]) == Blocks.air){
-            setRelMaybe([xo, yo, zo], Blocks.oak_leaves);
+          if(getRelOr([xo, yo, zo]) == Blocks.air){
+            setRelOr([xo, yo, zo], Blocks.oak_leaves);
           }
         }
       }
     }
     for(let offset=1;offset<=4;offset++){
-      setRelMaybe([0, offset, 0], Blocks.oak_log);
+      setRelOr([0, offset, 0], Blocks.oak_log);
     }
   }
 
