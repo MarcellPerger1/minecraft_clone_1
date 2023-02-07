ae9e016411473061ed0e8354b7856766b9d2cddb
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2023-02-07 21:19:31 +0000

    refactor(perf-test): Add meaningful names to stuff

--- perf/tree_gen/script.js
+++ perf/tree_gen/script.js
@@ -71,13 +71,13 @@ export function runSuite(suite) {
       startedAt = Date.now()/1000;
       console.log(`Benchmark ${i+1} done.`);
       
-      let p = makeElem(
+      let meanTime = makeElem(
         'td', [(b.stats.mean*1000).toFixed(3) + ' ms'], ['rjust', 'mono-font']);
-      let o = makeElem(
+      let opsPerSec = makeElem(
         'td', [(1 / b.stats.mean).toFixed(3) + ' ops/sec'], ['rjust', 'mono-font']);
-      let u = makeElem(
+      let uncertainty = makeElem(
         'td', ['+/-  ' + (b.stats.rme).toFixed(2) + '%'], ['rjust', 'mono-font']);
-      tr.append(p, o, u);
+      tr.append(meanTime, opsPerSec, uncertainty);
     });
   });
   suite.on('complete', () => {
@@ -85,21 +85,28 @@ export function runSuite(suite) {
     progDiv.remove();
     console.log("Done");
     console.log(suite);
+    
     let ordered = suite.map((v, i) => [v, i])
       .sort((a, b) => 1/b[0].stats.mean - 1/a[0].stats.mean);
     let [bestBench, bestRowIdx] = ordered[0];
     let bestOpsPerSec = 1/bestBench.stats.mean;
     let bestRow = rows[bestRowIdx];
-    let nth = makeElem("td", ["Fastest"], ["mono-font"]);
-    let slowerBy = makeElem("td", ["Fastest"], ["mono-font"]);
+    
+    let nth = makeElem(
+      "td", ["Fastest"], ["mono-font"]);
+    let slowerBy = makeElem(
+      "td", ["Fastest"], ["mono-font"]);
     bestRow.classList.add("perf-green");
     bestRow.append(nth, slowerBy);
+    
     for(let i=1;i<ordered.length;i++) {
       let [bench, rowIdx] = ordered[i];
       let row = rows[rowIdx];
+      
       row.classList.add((i == rows.length-1) ? "perf-red": "perf-amber");
       let nthText = (i+1) + (["st", "nd", "rd"][i+1] ?? "th");
-      let nth = makeElem("td", [nthText], ["mono-font"]);
+      let nth = makeElem(
+        "td", [nthText], ["mono-font"]);
       let opsPerSec = 1/bench.stats.mean;
       let fracOfBest = opsPerSec/bestOpsPerSec;
       let slowerBy = makeElem(
