190848c4adab63626dc2e67fe21b56eadd9043fa
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2023-04-08 09:22:46 +0000

    fix: Revert shaders to original state

--- shaders/fragment-shader.glsl
+++ shaders/fragment-shader.glsl
@@ -1,7 +1,7 @@
-//uniform sampler2D uSampler;
-//varying highp vec2 vTextureCoord;
+uniform sampler2D uSampler;
+varying highp vec2 vTextureCoord;
 
 
 void main(void) {
-  gl_FragColor = vec4(0.0, 0.2, 0.1, 1.0);//texture2D(uSampler, vTextureCoord);
+  gl_FragColor = texture2D(uSampler, vTextureCoord); //vec4(0.0, 0.2, 0.1, 1.0);
 }
--- shaders/vertex-shader.glsl
+++ shaders/vertex-shader.glsl
@@ -1,14 +1,14 @@
 attribute vec4 aVertexPosition;
-// attribute vec2 aTextureCoord;
+attribute vec2 aTextureCoord;
 
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
 // these are then passed to the fragment shader (interpolated between vertices)
-// varying highp vec2 vTextureCoord;
+varying highp vec2 vTextureCoord;
 
 
 void main(void) {
   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-  // vTextureCoord = aTextureCoord;
+  vTextureCoord = aTextureCoord;
 }
