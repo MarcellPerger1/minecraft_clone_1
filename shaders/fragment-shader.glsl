0b3d65471c17c4f8dccfeb7ad35b349ec46ebb6a
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2023-04-08 09:19:31 +0000

    refactor: Tidy up shaders

--- shaders/fragment-shader.glsl
+++ shaders/fragment-shader.glsl
@@ -3,6 +3,5 @@
 
 
 void main(void) {
-  //gl_FragColor = vColor;
   gl_FragColor = vec4(0.0, 0.2, 0.1, 1.0);//texture2D(uSampler, vTextureCoord);
 }
--- shaders/vertex-shader.glsl
+++ shaders/vertex-shader.glsl
@@ -1,23 +1,14 @@
 attribute vec4 aVertexPosition;
 // attribute vec2 aTextureCoord;
-//attribute vec4 aVertexColor;
 
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
 // these are then passed to the fragment shader (interpolated between vertices)
 // varying highp vec2 vTextureCoord;
-//varying lowp vec4 vColor;
 
 
 void main(void) {
   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-  // Yes, I know this line is weird but we need to tell the glsl compiler
-  // to stop messing with our code by removing 'unused' 
-  // (but for some weird reason still needed) attributes
-  // as `aTextureCoord` being removed results in 
-  // it mystreriously breaking. WHY ??? 
-  // Why is webgl (or perhaps my own code) so WEIRD ???
-  // vTextureCoord = aTextureCoord - aTextureCoord;
-  // vTextureCoord = vec2(0., 0.);
+  // vTextureCoord = aTextureCoord;
 }
