5b77925ac72a272f61df18eff85bdce283f11b0f
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2023-04-29 11:10:44 +0000

    fix: Fix unresolved reference

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -333,7 +333,7 @@ export class PickingIdRenderer extends MeshRenderer {
   makeRenderbuffer() {
     this.fb = this.gl.createFramebuffer();
     this.targetTexture = this.gl.createTexture();
-    this.gl.bindTexture(this.gl.TEXTURE_2D, targetTexture);
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.targetTexture);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
