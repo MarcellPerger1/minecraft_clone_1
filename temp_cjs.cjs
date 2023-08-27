8cb351d0ce77e4a3d31aa6eccadce0937ccf7222
Marcell Perger <marcell.perger.1@gmail.com>
2023-08-27 21:15:50 +0100

    fix: Remove some temp files

--- temp_cjs.cjs
+++
@@ -1,9 +0,0 @@
-exports.a = function a(v) {
-  var x = v;
-  if(v) {
-    x += v * x;
-  } else {
-    x += x * x - 3;
-  }
-  return x;
-}
--- temp_test.test.cjs
+++
@@ -1,7 +0,0 @@
-const x = require('./temp_cjs.cjs');
-
-describe("A", () => {
-  it("B", () => {
-    expect(x.a(1)).toBe(2);
-  })
-})
