69f99ba1c973f3026faa965949f55920b561b4cf
Marcell Perger <marcell.perger.1@gmail.com>
2023-08-30 21:34:28 +0100

    refactor: Add types

--- scripts/make_atlas.js
+++ scripts/make_atlas.js
@@ -3,12 +3,15 @@ import path from "node:path";
 import { pipeline } from "node:stream/promises";
 import { fileURLToPath } from 'node:url';
 
-import { createCanvas, loadImage } from "canvas";
+import nodeCanvas from "canvas";
 
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
+/**
+ * @param {string} dir
+ */
 async function getTexturePaths(dir) {
   let filenames = await fs.promises.readdir(dir);
   let paths = [];
@@ -27,12 +30,17 @@ function isTextureFile(f) {
 }
 
 /**
- * @param trustCwd {boolean} is cwd set properly (ie. to project root)
+ * @param {boolean} trustCwd is cwd set properly (ie. to project root)
  **/
 function getProjectRoot(trustCwd = true) {
   return trustCwd ? path.resolve() : path.resolve(dirname, "..");
 }
 
+/**
+ * @param {nodeCanvas.CanvasRenderingContext2D} ctx
+ * @param {nodeCanvas.Image} img
+ * @param {number} i
+ */
 function drawNthImage(ctx, img, i) {
   ctx.drawImage(img, i * 16, 0);
 }
@@ -41,19 +49,21 @@ function main() {
   const root = getProjectRoot();
   const resDir = path.resolve(root, "res/");
   const texDir = path.resolve(root, "textures/");
-  var canv, ctx, n;
+  var /** @type {nodeCanvas.Canvas} */canv;
+  var /** @type {nodeCanvas.CanvasRenderingContext2D} */ctx
+  var /** @type {number} */n;
   var i = 0;
   var data = [];
   console.log("Finding textures...");
   const ondrawn = getTexturePaths(texDir).then((paths) => {
     n = paths.length;
     console.log("Creating canvas...");
-    canv = createCanvas(n * 16, 16);
+    canv = nodeCanvas.createCanvas(n * 16, 16);
     ctx = canv.getContext("2d");
     console.log("Drawing images...");
     return Promise.all(
       paths.map((p) =>
-        loadImage(p).then((img) => {
+        nodeCanvas.loadImage(p).then((img) => {
           drawNthImage(ctx, img, i++);
           let nm = path.basename(p, ".min.png");
           data.push(nm);
@@ -77,6 +87,10 @@ function main() {
   });
 }
 
+/**
+ * @param {nodeCanvas.Canvas} canv
+ * @param {string} dir
+ */
 function writeToPng(canv, dir) {
   const out = fs.createWriteStream(dir);
   const png_stream = canv.createPNGStream();
