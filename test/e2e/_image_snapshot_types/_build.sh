d49a12883f86722a30d2d7d5d2b2200710da865c
Marcell Perger <marcell.perger.1@gmail.com>
2023-08-31 17:24:16 +0100

    fix: Remove unneeded install from `_build.sh`

--- test/e2e/_image_snapshot_types/_build.sh
+++ test/e2e/_image_snapshot_types/_build.sh
@@ -1,2 +1,1 @@
-npm i --no-save typescript && \
-  ./node_modules/.bin/tsc ./test/e2e/_image_snapshot_types/index.ts --lib es2023 -m nodenext -t esnext --declaration --skipLibCheck --strict
+./node_modules/.bin/tsc ./test/e2e/_image_snapshot_types/index.ts --lib es2023 -m nodenext -t esnext --declaration --skipLibCheck --strict
