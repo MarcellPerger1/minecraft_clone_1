03215c4ef53cec4021d18eea0d5301b08dd479b7
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-01 18:12:27 +0100

    style: Format code

--- test/loader_utils.test.js
+++ test/loader_utils.test.js
@@ -1,10 +1,10 @@
 import { expect, it, describe } from "@jest/globals";
 
-import {LoaderMerge, makeLoaderMerge} from "../src/utils/ts/loader_utils.js";
+import { LoaderMerge, makeLoaderMerge } from "../src/utils/ts/loader_utils.js";
 
 /**
  * @template {{[k: PropertyKey]: import('../src/utils/ts/loader_utils.d.ts').LoaderT}} L
- * @param {L} loaders 
+ * @param {L} loaders
  * @returns {LoaderMerge<L>}
  */
 function ctorLoaderMerge(loaders) {
@@ -13,11 +13,15 @@ function ctorLoaderMerge(loaders) {
 
 /**
  * @template T
- * @param {Promise<T>} promise 
+ * @param {Promise<T>} promise
  * @returns {import("../src/utils/ts/loader_utils.js").LoaderT<T>}
  */
 function makeLoader(promise) {
-  return { loadResources() { return promise; } };
+  return {
+    loadResources() {
+      return promise;
+    },
+  };
 }
 
 /**
@@ -28,46 +32,58 @@ async function wait(millis) {
 }
 
 describe.each([
-  {name: "LoaderMerge", fn: ctorLoaderMerge},
-  {name: "makeLoaderMerge.<return>", fn: makeLoaderMerge}
-])("$name", ({fn}) => {
+  { name: "LoaderMerge", fn: ctorLoaderMerge },
+  { name: "makeLoaderMerge.<return>", fn: makeLoaderMerge },
+])("$name", ({ fn }) => {
   it("Assigns loaders to the `.loaders` property", () => {
-    const loaders = {a: makeLoader(Promise.resolve(9)), b: makeLoader(Promise.resolve("abc"))};
+    const loaders = {
+      a: makeLoader(Promise.resolve(9)),
+      b: makeLoader(Promise.resolve("abc")),
+    };
     const lm = fn(loaders);
     expect(lm.loaders).toStrictEqual(loaders);
   });
   describe(".startPromises()", () => {
     it("Returns `this`", () => {
-      const loaders = {a: makeLoader(Promise.resolve(9)), b: makeLoader(Promise.resolve("abc"))};
+      const loaders = {
+        a: makeLoader(Promise.resolve(9)),
+        b: makeLoader(Promise.resolve("abc")),
+      };
       const lm = fn(loaders);
       expect(lm.startPromises()).toStrictEqual(lm);
     });
     it("Makes the `.promises` property", () => {
       var promiseA = Promise.resolve(9);
       var promiseB = Promise.resolve("abc");
-      const loaders = {a: makeLoader(promiseA), b: makeLoader(promiseB)};
+      const loaders = { a: makeLoader(promiseA), b: makeLoader(promiseB) };
       const lm = fn(loaders);
       lm.startPromises();
-      expect(lm.promises).toStrictEqual({a: promiseA, b: promiseB});
+      expect(lm.promises).toStrictEqual({ a: promiseA, b: promiseB });
     });
   });
   describe(".loadResources()", () => {
     it("Returns a promise (is async)", () => {
-      const loaders = {a: makeLoader(Promise.resolve(9)), b: makeLoader(Promise.resolve("abc"))};
+      const loaders = {
+        a: makeLoader(Promise.resolve(9)),
+        b: makeLoader(Promise.resolve("abc")),
+      };
       const lm = fn(loaders);
       lm.startPromises();
       expect(lm.loadResources()).toBeInstanceOf(Promise);
     });
     it("Resolves (with the correct value) when the single promise is resolved", async () => {
-      var /** @type {(value: any) => void} */resolvePromise, promiseDone = false;
+      var /** @type {(value: any) => void} */ resolvePromise,
+        promiseDone = false;
       const promise = new Promise((resolve) => {
         resolvePromise = resolve;
       });
-      const loaders = {p: makeLoader(promise)};
+      const loaders = { p: makeLoader(promise) };
       const lm = fn(loaders);
       lm.startPromises();
       const outPromise = lm.loadResources();
-      outPromise.then(() => { promiseDone = true; });
+      outPromise.then(() => {
+        promiseDone = true;
+      });
       await wait(40);
       // check that it doesn't resolve before resources loaded
       expect(promiseDone).toBe(false);
@@ -77,22 +93,33 @@ describe.each([
       // check that it does resolve after resources loaded
       expect(promiseDone).toBe(true);
       const returnValue = await outPromise;
-      expect(returnValue).toStrictEqual({p: resolveWith});
+      expect(returnValue).toStrictEqual({ p: resolveWith });
       expect(returnValue.p).toBe(resolveWith);
     });
     it("Resolves (with the correct value) only after all promises resolved", async () => {
-      var promiseDone = false, /** @type {{a: any, b: any}} */resolveValue;
+      var promiseDone = false,
+        /** @type {{a: any, b: any}} */ resolveValue;
       /** @type {{[k in ("a" | "b")]: {resolve?: (v: any) => void, value: any}}} */
-      const resolvers = {a: {value: {name: "a"}}, b: {value: {name: "b"}}};
-      const promises = {
-        a: new Promise((resolve) => { resolvers.a.resolve = resolve; }),
-        b: new Promise((resolve) => { resolvers.b.resolve = resolve; }),
+      const resolvers = {
+        a: { value: { name: "a" } },
+        b: { value: { name: "b" } },
       };
-      const loaders = {a: makeLoader(promises.a), b: makeLoader(promises.b)};
+      const promises = {
+        a: new Promise((resolve) => {
+          resolvers.a.resolve = resolve;
+        }),
+        b: new Promise((resolve) => {
+          resolvers.b.resolve = resolve;
+        }),
+      };
+      const loaders = { a: makeLoader(promises.a), b: makeLoader(promises.b) };
       const lm = fn(loaders);
       lm.startPromises();
       const outPromise = lm.loadResources();
-      outPromise.then((v) => { promiseDone = true; resolveValue = v; });
+      outPromise.then((v) => {
+        promiseDone = true;
+        resolveValue = v;
+      });
       // None of them done
       await wait(40);
       expect(promiseDone).toBe(false);
@@ -104,7 +131,10 @@ describe.each([
       resolvers.b.resolve(resolvers.b.value);
       await wait(40);
       expect(promiseDone).toBe(true);
-      expect(resolveValue).toStrictEqual({a: resolvers.a.value, b: resolvers.b.value});
+      expect(resolveValue).toStrictEqual({
+        a: resolvers.a.value,
+        b: resolvers.b.value,
+      });
       expect(resolveValue.a).toBe(resolvers.a.value);
       expect(resolveValue.b).toBe(resolvers.b.value);
     });
@@ -123,7 +153,10 @@ describe.each([
 
 describe("makeLoaderMerge.<return>", () => {
   it("Assigns loaders to returned object", () => {
-    const loaders = {a: makeLoader(Promise.resolve(13.169)), b: makeLoader(Promise.resolve("8910"))};
+    const loaders = {
+      a: makeLoader(Promise.resolve(13.169)),
+      b: makeLoader(Promise.resolve("8910")),
+    };
     const lm = makeLoaderMerge(loaders);
     expect(lm.a).toBe(loaders.a);
     expect(lm.b).toBe(loaders.b);
@@ -131,35 +164,50 @@ describe("makeLoaderMerge.<return>", () => {
 
   it("Doesn't assign blocked properties", () => {
     {
-      const loaders = {a: makeLoader(Promise.resolve(13.169)), promises: makeLoader(Promise.resolve("8910"))};
+      const loaders = {
+        a: makeLoader(Promise.resolve(13.169)),
+        promises: makeLoader(Promise.resolve("8910")),
+      };
       const lm = makeLoaderMerge(loaders);
       expect(lm.a).toBe(loaders.a);
-      expect(lm.promises).toBe(void 0);  // check that it wasn't assigned
+      expect(lm.promises).toBe(void 0); // check that it wasn't assigned
     }
     {
-      const loaders = {a: makeLoader(Promise.resolve(13.169)), toString: makeLoader(Promise.resolve("8910"))};
+      const loaders = {
+        a: makeLoader(Promise.resolve(13.169)),
+        toString: makeLoader(Promise.resolve("8910")),
+      };
       const toString = loaders.toString;
       const lm = makeLoaderMerge(loaders);
       expect(lm.a).toBe(loaders.a);
-      expect(lm.toString).not.toBe(toString);  // check that it wasn't assigned
+      expect(lm.toString).not.toBe(toString); // check that it wasn't assigned
     }
     {
-      const loaders = {a: makeLoader(Promise.resolve(13.169)), constructor: makeLoader(Promise.resolve("8910"))};
+      const loaders = {
+        a: makeLoader(Promise.resolve(13.169)),
+        constructor: makeLoader(Promise.resolve("8910")),
+      };
       const lm = makeLoaderMerge(loaders);
       expect(lm.a).toBe(loaders.a);
-      expect(lm.constructor).toBe(LoaderMerge);  // check that it wasn't assigned
+      expect(lm.constructor).toBe(LoaderMerge); // check that it wasn't assigned
     }
   });
   it("Doesn't modify the argument", async () => {
     const promiseA = Promise.resolve("123");
     const promiseB = Promise.resolve(-8.1);
-    const loadersOrig = {promises: makeLoader(promiseA), toString: makeLoader(promiseB)};
-    const loaders = {...loadersOrig}
+    const loadersOrig = {
+      promises: makeLoader(promiseA),
+      toString: makeLoader(promiseB),
+    };
+    const loaders = { ...loadersOrig };
     const lm = makeLoaderMerge(loaders);
     expect(loaders).toStrictEqual(loadersOrig);
     lm.startPromises();
-    expect(lm.promises).toStrictEqual({promises: promiseA, toString: promiseB});
+    expect(lm.promises).toStrictEqual({
+      promises: promiseA,
+      toString: promiseB,
+    });
     const returnValue = await lm.loadResources();
-    expect(returnValue).toStrictEqual({promises: "123", toString: -8.1});
+    expect(returnValue).toStrictEqual({ promises: "123", toString: -8.1 });
   });
 });
