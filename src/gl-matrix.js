b1d890960cc5b3468f1de47d6ee6713ef301ee5c
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-01 21:37:16 +0100

    fix: Update `src/gl-matrix.js`

--- src/gl-matrix.js
+++ src/gl-matrix.js
@@ -1,6 +1,3 @@
 // Just re-export all the gl-matrix stuff
-import * as _ from "./libs/gl-matrix/dist/gl-matrix.js";
-// @ts-ignore
-export const vec3 = self.vec3;
-// @ts-ignore
-export const mat4 = self.mat4;
+export * as vec3 from "./libs/gl-matrix/esm/vec3.js";
+export * as mat4 from "./libs/gl-matrix/esm/mat4.js";
