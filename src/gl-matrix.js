090f574220390109521c5bd13f3200a4c16c3331
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-01 21:06:44 +0100

    refactor: Add `src/gl-matrix` to re-export stuff

---
+++ src/gl-matrix.js
@@ -0,0 +1,6 @@
+// Just re-export all the gl-matrix stuff
+import * as _ from  "./libs/gl-matrix/dist/gl-matrix.js";
+// @ts-ignore
+export const vec3 = self.vec3;
+// @ts-ignore
+export const mat4 = self.mat4;
