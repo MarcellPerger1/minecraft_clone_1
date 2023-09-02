bab19f45cc113c1bdd5668e3d59ac77b023d340c
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-02 13:57:40 +0100

    feat: Added `block_to_place.ts`

---
+++ src/ts/block_to_place.ts
@@ -0,0 +1,35 @@
+import type { Player } from "../player.js";
+import { type BlockType, Blocks } from "../world/block_type.js";
+
+export class BlockToPlace {
+  player: Player;
+  selectElem: HTMLSelectElement;
+
+  constructor(player: Player) {
+    this.player = player;
+    this.selectElem = document.getElementById('select-block-to-place') as HTMLSelectElement;
+  }
+
+  loadBlocks() : this {
+    const placeholder = document.getElementById("loading-block-to-place");
+    placeholder.remove();
+
+    for(const blockType of Object.values(Blocks)) {
+      this.selectElem.appendChild(this.makeOptionForBlock(blockType));
+    }
+    return this;
+  }
+
+  makeOptionForBlock(block: BlockType) : HTMLOptionElement {
+    const elem = document.createElement("option");
+    elem.value = block.name;
+    elem.append(this._getDisplayName(block));
+    return elem;
+  }
+
+  _getDisplayName(block: BlockType) : string {
+    return block.name;  // later: convert snake_case to normal name
+  }
+
+
+}
