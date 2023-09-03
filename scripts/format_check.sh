79149ba77dbe79a4f145e10fa3770d2a5c8113a4
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-03 13:23:47 +0100

    fix: Add `.ts` files to prettier

--- scripts/format_check.sh
+++ scripts/format_check.sh
@@ -1,2 +1,2 @@
 shopt -s globstar
-npx prettier --check ./{src,perf,test,scripts}/**/*.{js,mjs,cjs} --no-error-on-unmatched-pattern "$@"
+npx prettier --check ./{src,perf,test,scripts}/**/*.{js,mjs,cjs,ts} --no-error-on-unmatched-pattern "$@"
