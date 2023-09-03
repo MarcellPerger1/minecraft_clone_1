ec0e3220fd5bd68e71b5f3fb66be94a4c0bb5b2a
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-03 11:30:11 +0100

    refactor: Add `process_coverage.js` to coverage

--- scripts/coverage-filtered.sh
+++ scripts/coverage-filtered.sh
@@ -1,1 +1,2 @@
 chmod +x ./scripts/test.sh && ./scripts/test.sh -c ./test/jest-config/jest-coverage-filtered.config.js "$@"
+node ./scripts/process_coverage.js
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,1 +1,2 @@
 chmod +x ./scripts/test.sh && ./scripts/test.sh -c ./test/jest-config/jest-coverage.config.js "$@"
+node ./scripts/process_coverage.js
