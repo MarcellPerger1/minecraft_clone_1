5bdaa6e450aa01d616ce7cf76fcf398d9ea6bdff
Marcell Perger <marcell.perger.1@gmail.com>
2024-07-07 19:19:35 +0100

    fix: Fix `linter.sh` exit code

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -1,7 +1,10 @@
 GREEN="\033[0;32m"
 NC="\033[0m"
 npx eslint --max-warnings 0 "$@" ./
-if test $? -eq 0
+ret=$?
+if test $ret -eq 0
 then
   echo -e "${GREEN}No errors found.${NC}"
+else
+  exit $ret  
 fi
