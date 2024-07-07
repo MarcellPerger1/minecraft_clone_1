bbf996065d24f248879bb397ee837c8c4d905227
Marcell Perger <marcell.perger.1@gmail.com>
2024-07-07 22:21:21 +0100

    fix: Move process_coverage into `scripts/test/cov`

--- scripts/test/cov/coverage-filtered.sh
+++ scripts/test/cov/coverage-filtered.sh
@@ -1,2 +1,2 @@
 ./scripts/test/test_base.sh --ci -c ./test/jest-config/jest-coverage-filtered.config.js "$@" &&
- node ./scripts/process_coverage.js
+ node ./scripts/test/cov/process_coverage.js
--- scripts/test/cov/coverage.sh
+++ scripts/test/cov/coverage.sh
@@ -1,2 +1,2 @@
 ./scripts/test/test_base.sh -c ./test/jest-config/jest-coverage.config.js "$@" &&
-  node ./scripts/process_coverage.js
+  node ./scripts/test/cov/process_coverage.js
